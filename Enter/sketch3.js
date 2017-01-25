f60341960fcf546b19cf56877baf3dbd13757f8e
srikanthBezawada <srikanth.5283@gmail.com>
2017-01-25 20:38:32 +0400

    mouseX, mouseY variables and mousePressed event gives a simple paint brush

---
+++ Enter/sketch3.js
@@ -0,0 +1,19 @@
+function setup() {
+	createCanvas(600, 400);
+	background(250, 250, 100); // Change this background to the starting of the draw() function and see what happens
+}
+
+function draw() {
+	noStroke();
+	fill(250, 200, 200, 50);
+	ellipse(mouseX, mouseY, 25, 25);
+	
+	//fill(200, 250, 200);
+	//rect(400, 100, 50, 50);
+}
+
+
+function mousePressed() { // execute when the mouse is pressed,
+	//yes, in that event of pressing take a break from draw() and execute here
+	background(250, 250, 100);// Erase the drawing
+}
