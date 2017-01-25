a47ac5dc1fe575eb297065133db650d74ed6c632
srikanthBezawada <srikanth.5283@gmail.com>
2017-01-25 20:58:18 +0400

    Adds self moving circle sketch, sketch4

---
+++ Enter/sketch4.js
@@ -0,0 +1,16 @@
+var circleX;
+
+function setup() {
+	createCanvas(600, 400);
+	circleX = 50
+}
+
+function draw() {
+	if(circleX > 600) {
+		circleX = circleX = 50;
+	}
+	background(250, 250, 100);
+	fill(250, 200, 200);
+	ellipse(circleX, 200, 200, 80);
+	circleX = circleX + 50;	
+}
